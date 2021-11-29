247daaadeb13e0792e0a7ad1f4a2fe51d3aa9184
Javier Flores <jffc.dev@gmail.com>
2021-11-29 02:12:58 -0500

    fix(users): Errors in create user functions solved

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,21 +4,25 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class UserProfileManager(BaseUserManager):
     """ Manager para perfiles de usuario """
-    def create_user(self, email, password=None):
+    def create_user(self, email, name, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
-        user = self.model(email=self.normalize_email(email),)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email=email, password=password,)
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
 
-        user.is_admin = True
-        user.is_active = True
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
+
         return user
 
 
