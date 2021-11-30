11e9f9cfe27e3cfb7a9ffb89f208ffdf64b471e1
Javier Flores <jffc.dev@gmail.com>
2021-11-30 00:48:57 -0500

    feat(api): First ApiView added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ API View de prueba """
+
+    def get(self, request, format=None):
+        """ Retornar lista de caracteristicas del API view"""
+        an_apiview = [
+            'Usamos métodos HTTP como funciones (get, post, path, put, delete)',
+            'Es similar a una vista tradicional de Django',
+            'Nos da el mayor control sobre la lógica de nuestra aplicación',
+            'Está mapeado manualmente a los URLs',
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
