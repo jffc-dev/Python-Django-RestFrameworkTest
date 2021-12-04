c1b224a3ae6bbe04d19495f58e57ace20831564e
Javier Flores <jffc.dev@gmail.com>
2021-12-04 12:12:09 -0500

    fix(db): Database deleted

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 
-from profiles_api import serializers
+from profiles_api import serializers, models
 
 
 class HelloApiView(APIView):
@@ -78,3 +78,9 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):
         return Response({'method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Crear y actualizar perfiles """
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
